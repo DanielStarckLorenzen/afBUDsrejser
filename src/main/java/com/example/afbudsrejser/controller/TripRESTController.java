fe96b38599e974a8781b933038902ccbc0df1629
daniellorenzen <dani141d@stud.kea.dk>
2023-05-02 11:02:16 +0200

    Changes by august

--- src/main/java/com/example/afbudsrejser/controller/TripRESTController.java
+++ src/main/java/com/example/afbudsrejser/controller/TripRESTController.java
@@ -1,7 +1,21 @@
 package com.example.afbudsrejser.controller;
 
+import com.example.afbudsrejser.model.Trip;
+import com.example.afbudsrejser.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TripRESTController {
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @GetMapping("/trips")
+    public List<Trip> getTrips(){
+      return tripRepository.findAll();
+    }
 }
--- src/main/java/com/example/afbudsrejser/controller/UserRESTController.java
+++ src/main/java/com/example/afbudsrejser/controller/UserRESTController.java
@@ -1,7 +1,14 @@
 package com.example.afbudsrejser.controller;
 
+import com.example.afbudsrejser.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserRESTController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
 }
